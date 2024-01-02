eadfc6c4891a30af5b0887c96808c8080bc3b806
pawas <pawas.saxena+1@gmail.com>
2024-01-02 17:28:01 +0530

    added a basic producer consumer pait

--- src/consumer.js
+++ src/consumer.js
@@ -1,22 +1,21 @@
 const amqplib = require("amqplib");
 
-const produce = async () => {
+const consumer = async () => {
   try {
     const queueName = "letterbox";
     const conn = await amqplib.connect("amqp://guest:guest@localhost:5672");
     const channel = await conn.createChannel();
     await channel.assertQueue(queueName);
 
-    await channel.sendToQueue(
-      queueName,
-      Buffer.from("alan turing was a smart guy")
-    );
+    const result = await channel.consume(queueName, (msg) => {
+      console.log("MSG", msg?.content.toString());
+      channel.ack(msg);
+    });
 
-    await channel.close();
-    await conn.close();
+    // not closing the connection
   } catch (err) {
     console.log("Err:", err);
   }
 };
 
-produce();
+consumer();
