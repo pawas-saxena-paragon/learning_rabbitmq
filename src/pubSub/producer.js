46fd8fae62d3039f8242317dab0f9f8c45d3bd6b
pawas <pawas.saxena+1@gmail.com>
2024-01-03 19:09:12 +0530

    added pub sub broadcast producer consumers

---
+++ src/pubSub/producer.js
@@ -0,0 +1,18 @@
+const { getConnection, PUB_SUB_EXCHANGE } = require("../utils/connection");
+
+const produce = async () => {
+  try {
+    const conn = await getConnection();
+    const channel = await conn.createChannel();
+
+    await channel.assertExchange(PUB_SUB_EXCHANGE, "fanout");
+
+    channel.publish(PUB_SUB_EXCHANGE, "", Buffer.from("Alan makes bad jokes"));
+    await channel.close();
+    await conn.close();
+  } catch (err) {
+    console.log("Err:", err);
+  }
+};
+
+produce();
