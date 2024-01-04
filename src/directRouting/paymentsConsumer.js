62c059ace0295bbf90b1973e35690b5c68746c5d
pawas <pawas.saxena+1@gmail.com>
2024-01-04 17:01:36 +0530

    added direct exchange for sending mesasges to a specified routing key queue

---
+++ src/directRouting/paymentsConsumer.js
@@ -0,0 +1,28 @@
+const {
+  getConnection,
+  ROUTING_EXCHANGE,
+  BOTH_ROUTING_KEY,
+  PAYMENTS_ONLY_KEY,
+} = require("../utils/connection");
+
+const consumer = async () => {
+  const conn = await getConnection();
+  const channel = await conn.createChannel();
+  await channel.assertExchange(ROUTING_EXCHANGE, "direct");
+  const { queue: randomQueueName } = await channel.assertQueue("", {
+    exclusive: true,
+  });
+
+  await channel.bindQueue(randomQueueName, ROUTING_EXCHANGE, PAYMENTS_ONLY_KEY);
+  await channel.bindQueue(randomQueueName, ROUTING_EXCHANGE, BOTH_ROUTING_KEY);
+
+  await channel.consume(
+    randomQueueName,
+    (msg) => {
+      console.log("mesasge in " + PAYMENTS_ONLY_KEY, msg?.content.toString());
+    },
+    { noAck: true }
+  );
+};
+
+consumer();
